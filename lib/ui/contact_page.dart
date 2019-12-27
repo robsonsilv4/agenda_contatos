242799db2241183bf10005bae19bc8650a80e08a
robsonsilv4 <robsonsilv410@gmail.com>
2019-12-27 17:24:17 -0300

    Interaçao com telefone e camera

--- lib/ui/contact_page.dart
+++ lib/ui/contact_page.dart
@@ -2,6 +2,7 @@ import 'dart:io';
 
 import 'package:agenda_contatos/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class ContactPage extends StatefulWidget {
   final Contact contact;
@@ -69,6 +70,19 @@ class _ContactPageState extends State<ContactPage> {
                     ),
                   ),
                 ),
+                onTap: () {
+                  ImagePicker.pickImage(
+                    source: ImageSource.camera,
+                  ).then((file) {
+                    if (file == null) {
+                      return;
+                    }
+
+                    setState(() {
+                      _editedContact.image = file.path;
+                    });
+                  });
+                },
               ),
               TextField(
                 decoration: InputDecoration(
